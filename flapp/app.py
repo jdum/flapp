2df991da63324a916676bbe67449f505c2c38542
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2023-08-08 09:46:30 +0200

    flapp v 0 1 2

--- flapp/app.py
+++ flapp/app.py
@@ -2,15 +2,14 @@
 import os
 import sys
 
-
-from flask import Flask
-
-
-import sqlalchemy
-
-
+import celery
+import click
+import numpy
+import pandas
 import redis
-
+import sqlalchemy
+import flask
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -22,18 +21,32 @@ REDIS_DB = 0
 
 @app.route("/")
 def hello_world():
-    if REDIS_SOCKET:
-        red = redis.Redis(unix_socket_path=REDIS_SOCKET)
-    else:
-        red = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
-    red.mset({"Croatia": "Zagreb", "France": "Paris"})
-    capital = red.get("France")
-    return (
-        "Hello World\n"
-        f"python version: {sys.version}\n"
-        f"Redis ({redis.__version__}) capital: {capital}\n"
-        f"sqlalchemy ({sqlalchemy.__version__})\n"
-    )
+    content = ["Hello World (0.1.2)"]
+    content.append(redis_test())
+    content.append(f"celery : {celery.__version__}")
+    content.append(f"click : {click.__version__}")
+    content.append(f"flask : {flask.__version__}")
+    content.append(f"numpy : {numpy.__version__}")
+    content.append(f"pandas : {pandas.__version__}")
+    content.append(f"python : {sys.version}")
+    content.append(f"sqlalchemy : {sqlalchemy.__version__}")
+
+    return "<br>".join(content)
+
+
+def redis_test() -> str:
+    response = f"Redis v({redis.__version__}) "
+    try:
+        if REDIS_SOCKET:
+            red = redis.Redis(unix_socket_path=REDIS_SOCKET)
+        else:
+            red = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+        red.mset({"Croatia": "Zagreb", "France": "Paris"})
+        capital = red.get("France")
+        response += f"France: {capital}"
+    except OSError as e:
+        response = str(e)
+    return response
 
 
 if __name__ == "__main__":
